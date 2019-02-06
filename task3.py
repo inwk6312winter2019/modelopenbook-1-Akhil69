117a36730df3e40c25d2e16b89ad8eac01caf37e
Akhil69 <Akhil.Golla@dal.ca>
2019-02-06 11:41:07 -0800

    task3

---
+++ task3.py
@@ -0,0 +1,21 @@
+def create_list():
+    myfile  = open("running-config.cfg","r")
+    transit_access_in = []
+    fw_management_access_in = []
+    global_access = []
+    for i in myfile:
+       if "transit_access_in" in i:
+           i = i.mystrip()
+           transit_access_in.append(i)
+       if "fw-management_access_in" in i:
+           i = i.strip()
+           fw_management_access_in.append(i)
+       if  "global_access" in i:
+           i = i.srip()
+           global_access.append(i)
+print("\ntransit_accesslist:" , transit_access_in)
+print("\fw management_list:" , fw_management_access_in)
+print("\global_access:" , global_access)
+create_list()
+
+
